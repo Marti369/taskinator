151fdf00c9bc02f2d69ec3faffe928864ae6d776
Edwin Mrtz <edwin.martinez.tech@gmail.com>
2020-10-07 23:09:01 -0700

    create element with JS, assign class, add textContent and appendChild to display on page and add event listener

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var buttonElement = document.querySelector("#save-task");
+var taskToDoElement = document.querySelector("#task-to-do");
+
+
+var createTaskHandler = function() {
+    var listItemElement = document.createElement('li'); //this creates a variable to create new element
+    listItemElement.className = "task-item"; //this assigns same class name for same style
+    listItemElement.textContent = "This is a new task." //this adds text Content
+    taskToDoElement.appendChild(listItemElement); //this appends child to show on page
+}
+
+buttonElement.addEventListener("click", createTaskHandler);
