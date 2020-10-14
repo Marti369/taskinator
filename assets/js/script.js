d802b58a7779cceb5dcdf57e9963363b7620e44d
Edwin Mrtz <edwin.martinez.tech@gmail.com>
2020-10-13 17:39:39 -0700

    add the event.preventDefault() function to stop the page from refreshing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,15 @@
-var buttonElement = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoElement = document.querySelector("#task-to-do");
 
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
     var listItemElement = document.createElement('li'); //this creates a variable to create new element
     listItemElement.className = "task-item"; //this assigns same class name for same style
     listItemElement.textContent = "This is a new task." //this adds text Content
     taskToDoElement.appendChild(listItemElement); //this appends child to show on page
+
 }
 
-buttonElement.addEventListener("click", createTaskHandler);
-
-
-
-
-
-
-continue on 4.2 .1 Introduction
+formEl.addEventListener("submit", createTaskHandler);
