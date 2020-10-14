2db84dd395774944f437508266b77f7e1a86679d
Edwin Mrtz <edwin.martinez.tech@gmail.com>
2020-10-13 19:32:51 -0700

    add varibles to create elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,36 @@ var formEl = document.querySelector("#task-form");
 var taskToDoElement = document.querySelector("#task-to-do");
 
 
+
 var createTaskHandler = function() {
 
-    event.preventDefault();
+    event.preventDefault(); //this prevents default of page refresh
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    taskToDoElement.appendChild(listItemEl);
+
+
     var listItemElement = document.createElement('li'); //this creates a variable to create new element
     listItemElement.className = "task-item"; //this assigns same class name for same style
     listItemElement.textContent = "This is a new task." //this adds text Content
     taskToDoElement.appendChild(listItemElement); //this appends child to show on page
+    listItemElement.textContent = taskNameInput;
 
 }
 
